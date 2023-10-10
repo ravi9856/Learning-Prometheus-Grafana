7a2e3989412af9774b16bd490a192e70612841b6
Ravindra Kumar <ravi.loveyouall@gmail.com>
2023-10-10 17:05:22 +0530

    Added Micrometer, prometheus and time dependencies

--- src/main/java/com/learning/LearningSpringboot/Controllers/BasicRestController.java
+++ src/main/java/com/learning/LearningSpringboot/Controllers/BasicRestController.java
@@ -1,5 +1,6 @@
 package com.learning.LearningSpringboot.Controllers;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BasicRestController {
 
     @GetMapping("/hello")
+    @Timed(value = "greeting.time", description = "Time taken to return greeting")
     public ResponseEntity<?> helloWorld(){
         log.info("Hello World!!");
+        System.out.println("Calling Add fn: " + add(5,3));
         return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
+    }
+
+//    @Timed(value = "add.time", description = "Time taken to return addition")
+    public int add(int a, int b){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return a+b;
     }
 }
--- src/main/java/com/learning/LearningSpringboot/LearningSpringbootApplication.java
+++ src/main/java/com/learning/LearningSpringboot/LearningSpringbootApplication.java
@@ -1,7 +1,10 @@
 package com.learning.LearningSpringboot;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearningSpringbootApplication {
@@ -10,4 +13,8 @@ public class LearningSpringbootApplication {
 		SpringApplication.run(LearningSpringbootApplication.class, args);
 	}
 
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
 }
