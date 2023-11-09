3eb83eae4da0263cc90a2e2fde531313e810388b
Ravindra Kumar <ravi.loveyouall@gmail.com>
2023-11-09 17:19:08 +0530

    Added prometheus yml file

--- src/main/java/com/learning/LearningSpringboot/Controllers/BasicRestController.java
+++ src/main/java/com/learning/LearningSpringboot/Controllers/BasicRestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 @RequestMapping(value = "/testController")
 @Slf4j
@@ -15,16 +18,20 @@ public class BasicRestController {
 
     @GetMapping("/hello")
     @Timed(value = "greeting.time", description = "Time taken to return greeting")
+    /*
+    If you don’t see your custom metric, make sure that you have triggered this function
+    at least once. The metric won’t appear until the timer has recorded at least once.
+     */
     public ResponseEntity<?> helloWorld(){
         log.info("Hello World!!");
-        System.out.println("Calling Add fn: " + add(5,3));
+        log.info("Calling Add fn: " + add(5,3));
         return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
     }
 
 //    @Timed(value = "add.time", description = "Time taken to return addition")
     public int add(int a, int b){
         try {
-            Thread.sleep(2000);
+            Thread.sleep(ThreadLocalRandom.current().nextInt(50, 2000));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
